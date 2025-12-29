970bf6f66b2e21e298303213c6bddb6d54a02d86
addy <addyjaime@gmail.com>
2025-12-29 18:03:39 -0500

    fase 1 parte 2 completada

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+
+// =======================================================
+// Representa UNA cuenta bancaria individual
+// Guarda únicamente información, no ejecuta acciones
+// =======================================================
+struct Bank_account
+{
+    int account_id;    
+    string user_name;  
+    double balance;    
+};
+
+// =======================================================
+// Representa el banco como entidad
+// El banco no es una cuenta, el banco POSEE cuentas
+// =======================================================
+struct Bank
+{
+    string bank_name;   
+
+    // Colección de cuentas bancarias
+    // Este vector permite almacenar múltiples cuentas
+    // Todas las cuentas son del tipo Bank_account
+    vector<Bank_account> accounts;
+};
 
 
 int main(){
