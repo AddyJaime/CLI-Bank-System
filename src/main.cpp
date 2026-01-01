4613b388f6cc414dd98e666fb375311e620234f0
addy <addyjaime@gmail.com>
2026-01-01 16:54:02 -0500

    Fase 2 parte 1 completada

--- src/main.cpp
+++ src/main.cpp
@@ -44,26 +44,64 @@ int main()
   my_bank.accounts.push_back(cuenta_1);
   my_bank.accounts.push_back(cuenta_2);
 
-  
-  int ID = 2;
-  bool found = false;
+   
 
-for (int i = 0; i < my_bank.accounts.size(); i++)
-{
-  if (ID == my_bank.accounts[i].account_id)
-  {
+ 
+
+ bool active_program = true;
+
+ while (active_program)
+ {
+
+   cout << "Menu: " << "\n";
+  // <-------------------------------------->
+  cout << "1:Crear cuenta" << "\n";
+  cout << "2:Ver cuenta" << "\n";
+  cout << "3:Salir" << "\n";
+ 
+   int user_number;
+ cout << "Please enter a number to continue:  \n";
+ cin >> user_number;
+
+ if (user_number == 1)
+ {
+  cout << "Crear cuenta Pendiendte" << "\n";
+ } else if (user_number == 2)
+ {
+  cout << "Ver cuenta (pendiente)"<< "\n";
+ } else if ( user_number == 3)
+ {
+  active_program = false;
+ } else
+ {
+   cout << "Opción no válida, intenta otra vez " << "\n";
+}
+ }
+ 
+
+
+
+
+
+//   int ID = 2;
+//   bool found = false;
+
+// for (int i = 0; i < my_bank.accounts.size(); i++)
+// {
+//   if (ID == my_bank.accounts[i].account_id)
+//   {
     
-    cout << "Account found: " << my_bank.accounts[i].user_name << "\n";
-    found = true;
-    break;
-  } 
+//     cout << "Account found: " << my_bank.accounts[i].user_name << "\n";
+//     found = true;
+//     break;
+//   } 
   
-}
+// }
 
-if (!found)
-{
-  cout << "Account not found \n";
-}
+// if (!found)
+// {
+//   cout << "Account not found \n";
+// }
 
 
   return 0;
